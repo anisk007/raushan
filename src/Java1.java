2d438a78dd4a8cdf66d1165de655e9dc161c96b4
Hido_Cydeo <hidoyatnuridinova@gmail>
2023-05-16 00:27:19 -0400

    Java class created

---
+++ src/Java1.java
@@ -0,0 +1,15 @@
+public class Java1 {
+    public static void main(String[] args) {
+
+        String result = "";
+        int n = 8;
+        if(n <= 8){
+            result = "Good";
+        }else{
+            result = "Bad";
+        }
+        System.out.println(result);
+
+
+    }
+}
